8f3f8f60ba9f9f73efe737fab88f49119ef73e6c
Thiago Adorno <thiago.adorno@aceflex.com.br>
2024-09-16 12:39:37 -0300

    Implementing simple search and update Bootstrap

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -1,17 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordsService _salesRecordsService;
+
+        public SalesRecordsController(SalesRecordsService salesRecordsService)
+        {
+            _salesRecordsService = salesRecordsService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? firstDate, DateTime? lastDate)
         {
-            return View();
+            if (firstDate == null)
+            {
+                firstDate = DateTime.Parse("01/01/1000");
+            }
+            if (!lastDate.HasValue)
+            {
+                lastDate = new DateTime(9999, 12, 31);
+            }
+
+            ViewData["firstDate"] = firstDate.Value.ToString("yyyy-MM-dd");
+            ViewData["lastDate"] = lastDate.Value.ToString("yyyy-MM-dd");
+            var sales = await _salesRecordsService.FindByDateAsync(firstDate, lastDate);
+
+            return View(sales);
         }
 
         public IActionResult GroupingSearch()
---
+++ SalesWebMvc/Services/SalesRecordsService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordsService
+    {
+        private readonly SalesWebMvcContext _salesContext;
+
+        public SalesRecordsService(SalesWebMvcContext salesContext)
+        {
+            _salesContext = salesContext;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? firstDate, DateTime? lastDate)
+        {
+            var sales = await _salesContext
+                                .SalesRecord
+                                .Include(x => x.Seller)
+                                .Include(x => x.Seller.Department)
+                                .Where(sale => sale.Date >= firstDate && sale.Date <= lastDate)
+                                .OrderByDescending(sale => sale.Date)
+                                .ToListAsync();
+
+            return sales;
+        }
+    }
+}
